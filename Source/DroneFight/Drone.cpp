61e26d4609b92e5f506ab044861aee356f49fe84
yxqin <2410030025@qq.com>
2022-07-17 16:34:18 +0800

    初步完成Drone

--- Source/DroneFight/Drone.cpp
+++ Source/DroneFight/Drone.cpp
@@ -2,12 +2,50 @@
 
 
 #include "Drone.h"
-
-// Sets default values
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "PhysicsEngine/PhysicsThrusterComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 ADrone::ADrone()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true; //设置每一帧都调用tick
+	
+	/*碰撞体*/
+	OutCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("OutCollision"));
+	RootComponent = OutCollision;//作为Root元素
+	OutCollision->SetBoxExtent(FVector(90,90,40));//设置大小
+	OutCollision->SetSimulatePhysics(true);//开启物理
+	OutCollision->SetCollisionProfileName(TEXT("Pawn"));
+	//锁定X Y 旋转，防止发生倾斜
+	OutCollision->BodyInstance.bLockXRotation = true;
+	OutCollision->BodyInstance.bLockYRotation = true;
+
+	/*设置无人机模型*/
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));//无人机主体
+	Mesh->SetupAttachment(OutCollision);
+	Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 25.0f));
+	Paddle1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle1"));//飞翼
+	Paddle2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle2"));
+	Paddle3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle3"));
+	Paddle4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle4"));
+	//飞翼挂在 到无人机上
+	Paddle1->SetupAttachment(Mesh, TEXT("Paddle1"));
+	Paddle2->SetupAttachment(Mesh, TEXT("Paddle2"));
+	Paddle3->SetupAttachment(Mesh, TEXT("Paddle3"));
+	Paddle4->SetupAttachment(Mesh, TEXT("Paddle4"));
+
+	/*设置推进器*/
+	UpThruster = CreateDefaultSubobject<UPhysicsThrusterComponent>("UpThruster");
+	UpThruster->SetupAttachment(RootComponent);
+	UpThruster->ThrustStrength = 980.0f;
+	UpThruster->SetAutoActivate(true);
+	UpThruster->SetWorldRotation(UKismetMathLibrary::MakeRotFromX(-GetActorUpVector()));//旋转到向下
+
+	ForwardThruster = CreateDefaultSubobject<UPhysicsThrusterComponent>("ForwardThruster");
+	ForwardThruster->SetupAttachment(RootComponent);
+	ForwardThruster->ThrustStrength = 0.0f;
+	ForwardThruster->SetAutoActivate(true);
+	ForwardThruster->SetWorldRotation(UKismetMathLibrary::MakeRotFromX(-GetActorForwardVector()));//旋转到向下
 
 }
 
@@ -15,14 +53,13 @@ ADrone::ADrone()
 void ADrone::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
 void ADrone::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
--- Source/DroneFight/Drone.h
+++ Source/DroneFight/Drone.h
@@ -6,26 +6,53 @@
 #include "GameFramework/Pawn.h"
 #include "Drone.generated.h"
 
-// 无人机类
+/**
+* @brief 无人机类
+**/
 
+
+class UStaticMeshComponent;
+class UBoxComponent;
+class UPhysicsThrusterComponent;
 UCLASS()
 class DRONEFIGHT_API ADrone : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ADrone();
-
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	/*属性成员*/
+
+
+	//碰撞盒子
+	UPROPERTY(VisibleAnywhere)
+		UBoxComponent* OutCollision;
+
+	//无人机
+	UPROPERTY(VisibleAnywhere) //提供垃圾回收机制、可以在虚幻引擎中访问、而且提供序列化能力
+		UStaticMeshComponent* Mesh;
+
+	//无人机飞翼 x 4
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* Paddle1;
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* Paddle2;
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* Paddle3;
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* Paddle4;
+
+	//推进器
+	UPROPERTY(VisibleAnywhere)
+		UPhysicsThrusterComponent* UpThruster;
+	UPROPERTY(VisibleAnywhere)
+		UPhysicsThrusterComponent* ForwardThruster;
 
 };
