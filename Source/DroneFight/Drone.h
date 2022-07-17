3adb65aac89d9ee484f3fe555c1412e8707b32e6
yxqin <2410030025@qq.com>
2022-07-17 13:58:39 +0800

    添加MainWorld基础关卡

---
+++ Source/DroneFight/Drone.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Drone.generated.h"
+
+// 无人机类
+
+UCLASS()
+class DRONEFIGHT_API ADrone : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADrone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
